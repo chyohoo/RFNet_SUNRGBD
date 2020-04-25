937aa1b7482dd310bdfe2489cdb2302bce14681b
chyohoo <chyohoo@gmail.com>
2020-04-25 06:55:19 +0200

    update

--- mypath.py
+++ mypath.py
@@ -2,7 +2,7 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'SUNRGBD':
-            return 'E:\\datasets\\cityscapes\\'      # folder that contains leftImg8bit/
+            return '/home/hchen/dataset_sun'      # folder that contains leftImg8bit/
          # folder that mixes Cityscapes and Lost and Found
         else:
             print('Dataset {} not available.'.format(dataset))
