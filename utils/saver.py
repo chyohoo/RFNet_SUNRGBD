e04d0873449dc5cd7d31193a955604892a7818f5
chyohoo <chyohoo@gmail.com>
2020-04-26 04:22:06 +0200

    update

--- utils/saver.py
+++ utils/saver.py
@@ -51,8 +51,8 @@ class Saver(object):
         p['lr_scheduler'] = self.args.lr_scheduler
         p['loss_type'] = self.args.loss_type
         p['epoch'] = self.args.epochs
-        p['base_size'] = self.args.base_size
-        p['crop_size'] = self.args.crop_size
+        # p['base_size'] = self.args.base_size
+        # p['crop_size'] = self.args.crop_size
 
         for key, val in p.items():
             log_file.write(key + ':' + str(val) + '\n')
