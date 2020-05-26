f7d9c201ce5168ef888dbe045e9de59e04640396
chyohoo <chyohoo@gmail.com>
2020-05-26 04:38:28 +0200

    update

--- toScript.py
+++ toScript.py
@@ -18,7 +18,7 @@ def load_my_state_dict(model, state_dict):  # custom function to load model when
 resnet_ = resnet18(pretrained=True, efficient=False, use_bn= True)
 model_ = RFNet(resnet_,num_classes=37, use_bn= True)
 # model_ = torch.nn.DataParallel(model_)   #并不支持trace nn.
-model_ =model_.cuda()
+
 
 device_ = torch.device('cpu')
 new_state_dict_ = torch.load('./model_best.pth',map_location=device_)
@@ -34,8 +34,8 @@ model_.eval()
 # depth_shape torch.Size([8, 1, 480, 640])
 # 记得归一化图像 /255
 
-image_example = torch.rand(1,3,480,640).cuda()
-depth_example = torch.rand(1,1,480,640).cuda() #保存时的模型使用cuda输入 此处也必须为cuda
+image_example = torch.rand(1,3,480,640)
+depth_example = torch.rand(1,1,480,640) #保存时的模型使用cuda输入 此处也必须为cuda
 
 with torch.no_grad():
     traced_model = torch.jit.trace(model_,(image_example,depth_example))
